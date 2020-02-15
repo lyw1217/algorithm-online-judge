7b8de4eea80df3cfefb1ef4d760dd205a90fbfe7
lyw1217 <mvl100d@gmail.com>
2020-02-16 02:34:29 +0900

    v1.0

---
+++ 1157.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+char solution(string str) {
+	int alphabet[26] = { 0, };
+	int max = -1;
+	int count = 0;
+	char answer;
+
+	int str_len = str.length();
+
+	for (int i = 0; i < str_len; i++) {
+		if (str[i] >= 'a' && str[i] <= 'z') {
+			str[i] -= 32;
+		}
+		alphabet[(str[i] - 65)]++;
+	}
+
+	for (int i = 0; i < 26; i++) {
+		if (alphabet[i] > max) {
+			max = alphabet[i];
+		}
+	}
+
+	for (int i = 0; i < 26; i++) {
+		if (alphabet[i] == max) {
+			answer = i + 65;
+			count++;
+		}
+	}
+
+	return (count > 1) ? '?' : answer;
+}
+
+int main() {
+	string str;
+
+	cin >> str;
+
+	cout << solution(str);
+
+	return 0;
+}
