23a631b0eefe93148965c396a97fa936e35c8821
lyw1217 <mvl100d@gmail.com>
2020-02-16 01:37:28 +0900

    v1.0

---
+++ 2675.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+	char str[21] = {'\0',};
+	int test_case;
+	int repetition;
+	
+	scanf("%d", &test_case);
+	
+	for(int i = 0; i < test_case; i++){
+		scanf("%d %s", &repetition, str);
+		
+		for(int j = 0; j < strlen(str); j++){
+			for(int k = 0; k < repetition; k++){
+				printf("%c", str[j]);
+			}
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
