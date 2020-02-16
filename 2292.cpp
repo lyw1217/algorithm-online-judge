b8d72fbb8cef15eb979b4d407db7f5e48ac593ed
lyw1217 <mvl100d@gmail.com>
2020-02-17 00:04:13 +0900

    v1.1

--- 2292.cpp
+++ 2292.cpp
@@ -1,30 +1,27 @@
 #include<iostream>
-#include<algorithm>
 
 using namespace std;
 
-long long int sum(long long int n) {
-	long long int sum = 0;
-	for (int i = 0; i < n; i++) {
-		sum += i;
-	}
-	return sum;
-}
-
 int main() {
 
-	/* 
-		각 레이어별로 생각.
-		가운데를 1번, 2~7을 2번, 8~19을 3번 레이어...
-		레이어의 시작점 : 6 * sum(0 ~ n-1) + 1
-		시작점이 num보다 더 커지면 그 때의 i값이 num이 속한 레이어
+	/*
+		각 레이어 별로 몇 칸씩 차지하고 있는지 세보자.
+		1 - 1
+		2 - 6
+		3 - 12
+		4 - 18
+		...
+		6의 배수만큼씩 차지하고 있으니 n이 sum(6*i)보다 작거나 같아지는 순간의 (i+1)값이
+		n이 자리하고 있는 레이어의 번호가 된다.
 	*/
-	long long int num;
+	long long int num, temp;
 	cin >> num;
-	
-	for (int i = 1;; i++) {
-		if ((6 * sum(i) + 1) >= num) {
-			cout << i;
+	temp = 1;
+	for (long long int i = 0;; i++) {
+		temp += 6 * i;
+		cout << "temp : " << temp << endl;
+		if (temp >= num) {
+			cout << i + 1;
 			break;
 		}
 	}
