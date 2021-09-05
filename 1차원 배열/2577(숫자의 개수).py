ec4093d6902c8c8943cb0576eead8aaf9016391f
lyw1217 <mvl100d@gmail.com>
2021-09-05 23:05:00 +0900

    v1.1

--- 1차원 배열/2577(숫자의 개수).py
+++ 1차원 배열/2577(숫자의 개수).py
@@ -3,7 +3,7 @@
 num = 1
 for i in range(3) :
     num *= int(input())
-
+'''
 s_num = str(num)
 line = [0,0,0,0,0,0,0,0,0,0]
 for i in range(len(s_num)) :
@@ -31,3 +31,17 @@ for i in range(len(s_num)) :
 
 for i in line :
     print(i)
+'''
+line = [0,0,0,0,0,0,0,0,0,0]
+while num :
+    line[int(num%10)] += 1
+    num = int(num / 10)
+
+for i in line :
+    print(i)
+
+'''
+참고한 자료
+제출 번호	아이디	문제	문제 제목	결과	메모리	시간	언어	코드 길이	제출한 시간
+21093912	gs20036	2577	숫자의 개수	맞았습니다!!	1112	0	C++14	157	1년 전
+'''
