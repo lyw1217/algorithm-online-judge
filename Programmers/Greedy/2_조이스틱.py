3b1dbc4e0cd3b0ce7da4f6583da7c59d559785b5
lyw1217 <mvl100d@gmail.com>
2021-09-17 00:12:53 +0900

    v0.2

--- Programmers/Greedy/2_조이스틱.py
+++ Programmers/Greedy/2_조이스틱.py
@@ -27,9 +27,8 @@ name의 길이는 1 이상 20 이하입니다.
 
 좌우 방향키로 커서를 최소로 이동하는 방법
 1. 오른쪽으로 쭉 이동하는 비용 = len(name) (만약 나머지가 전부 A라면, 전체 count에서 'A'이동 count 뺴기)
-2. A가 나올 때까지 커서를 오른쪽으로 이동하고 반대 방향으로 다시 되돌아가는 비용
-3. 왼쪽으로 쭉 이동하는 비용 = len(name) (만약 나머지가 전부 A라면, 전체 count에서 'A'이동 count 뺴기)
-4. A가 나올 때까지 커서를 왼쪽으로 이동하고 반대 방향으로 다시 되돌아가는 비용
+2. 왼쪽으로 쭉 이동하는 비용 = len(name) (만약 나머지가 전부 A라면, 전체 count에서 'A'이동 count 뺴기)
+
 '''
 
 #           0     1    2    3    4    5    6    7    8    9    10   11   12  13   14   15   16   17   18   19   20   21   22   23   24   25  
@@ -43,10 +42,10 @@ def solution(name) :
     flag            = 0 # A가 아닌 값이 나왔을 때 1
     a_count         = 0 # A인 곳을 지나가는 count
 
-    print('name = ', name)
+    #print('name = ', name)
 
     for idx in name :
-        print(idx)
+        #print(idx)
         ''' 상하 방향키 '''
         # 알파벳 전체의 절반보다 앞쪽에 있으면 그대로
         if alphabet.index(idx) <= half :
@@ -58,7 +57,7 @@ def solution(name) :
             alpha_count += abs(alphabet.index(idx) - alpha_len)
 
     ''' 좌우 방향키 '''
-    # 1. 오른쪽으로 쭉 이동하는 비용 = len(name) (만약 나머지가 전부 A라면, 전체 count에서 'A'이동 count 뺴기)
+    # 1. 오른쪽으로 쭉 이동하는 비용 = len(name)-1 (만약 나머지가 전부 A라면, 전체 count에서 'A'이동 count 뺴기)
     for i in range(len(name)) :
         if name[i] == 'A' :
             a_count += 1
@@ -71,13 +70,11 @@ def solution(name) :
         cursor_count.append(len(name)-1 - a_count)
     else :
         cursor_count.append(len(name)-1)
+    
+    a_count = 0
+    flag    = 0
 
-    # 2. A가 나올 때까지 커서를 오른쪽으로 이동하고 반대 방향으로 다시 되돌아가는 비용
-    # A가 하나 나오면 무조건 반대 방향으로 되돌아가버림
-    #for idx in name :
-    #    if idx == 'A' :
-
-    # 3. 왼쪽으로 쭉 이동하는 비용 = len(name) (만약 나머지가 전부 A라면, 전체 count에서 'A'이동 count 뺴기)
+    # 2. 왼쪽으로 쭉 이동하는 비용 = len(name)-1 (만약 나머지가 전부 A라면, 전체 count에서 'A'이동 count 뺴기)
     for i in range(len(name)) :
         if name[-i] == 'A' :
             a_count += 1
@@ -85,22 +82,33 @@ def solution(name) :
         else :
             a_count  = 0
             flag     = 1
-        print('name[{}] = {} '.format(-i,name[-i]))
     
     if flag == 0 :
         cursor_count.append(len(name)-1 - a_count)
     else :
         cursor_count.append(len(name)-1)
     
-
-    # 4. A가 나올 때까지 커서를 왼쪽으로 이동하고 반대 방향으로 다시 되돌아가는 비용
-    #for idx in reversed(name) :
-    #    if idx == 'A' :
-
+    # 3. 오른쪽으로 이동 중 'A'가 나오면 뒤로 back해서 [다시 A가 나올때까지 or i+1인덱스까지]
+    for i in range(len(name)) :
+        if (name[i] == 'A') and (i <= (len(name) // 2)) :
+            back_count = 0
+            for j in range(len(name)-1, -(i) , -1) :
+                #print('name[{}] = {}'.format(j, name[j]))
+                back_count += 1
+                #print(back_count)
+                if name[j] == 'A' or j == -(i-2) :
+                    if ((i - 1) + (i - 1) + (back_count - 1)) > 0 :
+                        cursor_count.append((i - 1) + (i - 1) + (back_count - 1))
+                        #print('i({}) + i-1({}) + back_count-1({}) = {}'.format(i-1, i-1, back_count-1, i + (i-1) + (back_count-1)))
+                    break
+            #break
+            
     return alpha_count + min(cursor_count)
 
 if __name__ == "__main__":
-    name = ["JAZ", "JEROEN", "JAN"]
+    name = ["JAZ", "JEROEN", "JAN", "ABAAAAAAAAABB", "ZAAAZZZZZZZ"]
+    # "ABAAAAAAAAABB", 11번 테스트 케이스, 답 : 7
+    # "ZAAAZZZZZZZ" 답 : 15
     result = 0
     for i in range(len(name)) :
         result = solution(name[i])
@@ -120,21 +128,32 @@ if __name__ == "__main__":
                 print( 'SUCCESS' )
             else :
                 print( 'FAIL' )
+        elif i == 3 :
+            if result == 7 :
+                print( 'SUCCESS' )
+            else :
+                print( 'FAIL' )
+        elif i == 4 :
+            if result == 15 :
+                print( 'SUCCESS' )
+            else :
+                print( 'FAIL' )
 
 
 '''
-테스트 1 〉	통과 (0.01ms, 10.2MB)
-테스트 2 〉	통과 (0.02ms, 10.3MB)
-테스트 3 〉	통과 (0.01ms, 10.3MB)
-테스트 4 〉	통과 (0.03ms, 10.3MB)
-테스트 5 〉	통과 (0.03ms, 10.3MB)
-테스트 6 〉	통과 (0.02ms, 10.3MB)
-테스트 7 〉	통과 (0.02ms, 10.2MB)
-테스트 8 〉	통과 (0.02ms, 10.2MB)
-테스트 9 〉	통과 (0.02ms, 10.1MB)
+테스트 1 〉	통과 (0.01ms, 10.3MB)
+테스트 2 〉	통과 (0.01ms, 10.3MB)
+테스트 3 〉	통과 (0.01ms, 10.2MB)
+테스트 4 〉	실패 (0.02ms, 10.3MB)
+테스트 5 〉	통과 (0.02ms, 10.4MB)
+테스트 6 〉	통과 (0.01ms, 10.4MB)
+테스트 7 〉	실패 (0.02ms, 10.3MB)
+테스트 8 〉	통과 (0.01ms, 10.4MB)
+테스트 9 〉	통과 (0.01ms, 10.4MB)
 테스트 10 〉	통과 (0.02ms, 10.3MB)
-테스트 11 〉	실패 (0.02ms, 10.2MB)
+테스트 11 〉	통과 (0.02ms, 10.3MB)
 
-한쪽 방향으로만 처리하게 해도 테스트 11만 실패한다.
-테스트를 다양하게 하지는 않는 것 같다.
+테스트 11을 통과하니까
+테스트 4,7이 실패한다..
+새로 추가한 # 3에서 잘못 도는 듯
 '''
