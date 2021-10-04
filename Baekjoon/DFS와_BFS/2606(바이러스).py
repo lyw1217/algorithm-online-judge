5a1e5799a942226a8fc3bf95e7b9f044d31545e0
lyw1217 <mvl100d@gmail.com>
2021-10-04 21:54:43 +0900

    v1.0

--- Baekjoon/DFS와_BFS/2606(바이러스).py
+++ Baekjoon/DFS와_BFS/2606(바이러스).py
@@ -3,28 +3,29 @@ from collections import deque
 
 n = int(input()) # 컴퓨터의 수
 graph = {}
+
+for i in range(n) :
+    graph[i+1] = set()
+
 edge = int(input()) # 직접 연결되어 있는 컴퓨터 쌍의 수
 
-for i in range(1, edge+1) :
-    graph[i] = set((map(int, input().split(' '))))
-
-print(graph)
+for i in range(edge) :
+    a, b = (map(int, input().split(' ')))
+    graph[a].add(b)
+    graph[b].add(a)
 
 def bfs(graph, root) :
     queue   = deque([root])    # deque
-    visited = []    # 방문한 노드
+    visited = [ ]    # 방문한 노드
     
     while queue :
         cur_node = queue.popleft()
 
-        if cur_node not in visited :
-            visited.append(cur_node)
-            queue += graph[cur_node] - set(visited)
-            print('queue = ', queue)
-            print('graph[cur_node] = ', graph[cur_node])
-            print('visited = ' , set(visited))
-    
+        for i in graph[cur_node] :
+            if i not in visited :
+                queue.append(i)
+                visited.append(i)
+   
     return visited
 
-print(len(bfs(graph, 1)))
-
+print(len(bfs(graph, 1))-1)
