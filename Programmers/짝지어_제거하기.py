a4809f9c16490616c033d1bafe2a3285e04f43dc
Youngwoo Lee <mvl100d@gmail.com>
2021-10-08 20:02:20 +0900

    v1.0

--- Programmers/짝지어_제거하기.py
+++ Programmers/짝지어_제거하기.py
@@ -1,34 +1,19 @@
 def solution(s):
+    if len(s) % 2 != 0 :
+        return 0
     answer = -1
-    i = 1
-    s = list(s)
-    flag = 0
-    while True :
-        s_len = len(s)
-        if i >= s_len :
-            if flag == 1 :
-                i = 1
-                flag = 0
-                continue
-            else :
-                if s_len == 0 :
-                    answer = 1
-                else :
-                    answer = 0
-                break
+    
+    stack = [s[0]]
 
-        if s_len > 1 and s[i] == s[i-1] :
-            print('s[i] = {} s[i-1] = {}'.format(s[i], s[i-1]))
-            del s[i-1]
-            print('s[i] = {} s[i-1] = {}'.format(s[i], s[i-1]))
-            del s[i-1]
-            print("")
-            i -= 1
-            if i < 0 : i = 0
-            flag = 1
-        else : 
-            i += 1
-
+    for i in s[1:] :
+        if len(stack) > 0 and stack[-1] == i :
+            stack.pop()
+        else :
+            stack.append(i)
+    if len(stack) == 0 :
+        answer = 1
+    else :
+        answer = 0
     return answer
 
 print(solution("baabaa"))
