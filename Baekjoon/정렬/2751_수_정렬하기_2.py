6471e3a74f0ee6ca00c527f5fa05615e84932deb
lyw1217 <mvl100d@gmail.com>
2021-11-04 22:16:52 +0900

    v1.2

--- Baekjoon/정렬/2751_수_정렬하기_2.py
+++ Baekjoon/정렬/2751_수_정렬하기_2.py
@@ -9,5 +9,6 @@ num = sorted(num)
 result = ''
 for i in num :
     #print(i)    # 한 줄 씩 출력하는 것보다 문자열에 더하는 것이 더 빠르다 1488ms -> 1400ms
-    result += (str(i) + '\n')
+    #result += (str(i) + '\n')
+    result += f'{i}\n'  # + 연산자를 사용하는 것보다 f-string 사용하는 것이 더 빠르다. 1400ms->1288ms
 print(result)
